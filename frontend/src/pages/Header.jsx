28845266334ca57bd3041e50481377cb8eb3988a
Deepak <deepakraj172004@gmail.com>
2026-05-28 20:52:11 +0530

    latest fix2

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "../styles/header.css";
+import "../styles/Header.css";
 
 export default function Header() {
   const isLoggedIn = !!localStorage.getItem("token");
